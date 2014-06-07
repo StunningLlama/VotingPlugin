bc17a8f1df6f2fc2c19b0243a35c4203d9bf87ef
2insanepeople <zcm727@gmail.com>
2014-06-07 10:08:18 -0500

    Create PlayerLoginHandler.java

---
+++ src/main/java/io/github/thepowderguy/ZweiAndPowdersVotePlugin/PlayerLoginHandler.java
@@ -0,0 +1,25 @@
+package io.github.thepowderguy.ZweiAndPowdersVotePlugin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+
+public final class PlayerLoginHandler implements Listener {
+	ZweiAndPowdersVotePlugin plugin;
+	public PlayerLoginHandler(ZweiAndPowdersVotePlugin instance)
+	{
+		plugin = instance;
+	}
+	@EventHandler
+	public void onLogin(PlayerLoginEvent event)
+	{
+		int currentId = plugin.getConfig().getInt("current-id");
+		if(plugin.getConfig().getInt("votes." + currentId + ".time-end") > System.currentTimeMillis() / 1000L)
+		{
+			event.getPlayer().sendMessage(ChatColor.AQUA + "There is a vote going on! The question is:");
+			event.getPlayer().sendMessage(ChatColor.GRAY + plugin.getConfig().getString("votes." + currentId + ".question"));
+			event.getPlayer().sendMessage(ChatColor.RED + "Do /vote to see options, and do /vote (choice) to vote!");
+		}
+	}
+}
