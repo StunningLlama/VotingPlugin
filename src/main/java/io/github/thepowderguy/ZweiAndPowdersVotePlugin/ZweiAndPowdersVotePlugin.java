fdcff931155294605fc974965baa2ef2b3206097
thepowderguy <brandonli2010@gmail.com>
2014-06-07 08:20:46 -0400

    Update ZweiAndPowdersVotePlugin.java

--- src/main/java/io/github/thepowderguy/ZweiAndPowdersVotePlugin/ZweiAndPowdersVotePlugin.java
+++ src/main/java/io/github/thepowderguy/ZweiAndPowdersVotePlugin/ZweiAndPowdersVotePlugin.java
@@ -18,6 +18,17 @@ public final class ZweiAndPowdersVotePlugin extends JavaPlugin {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
+		if(cmd.getName().equalsIgnoreCase("createvote"))
+		{
+			String[] choices = args[1].split(",");
+ +			String[] disallowed;
+ +			if (args[2].equalsIgnoreCase("none"))
+ +				disallowed = null;
+ +			else
+ +				disallowed = args[2].split(",");
+ +			long endtime = (long) ((System.currentTimeMillis() / 1000L) + (Double.valueOf(args[0]) * 3600));
+ +		  //TODO
+		}
 		if(cmd.getName().equalsIgnoreCase("vote"))
 		{
 			if(args.length == 0)
