02a200e4bbcf86d90a7c3209b30ec4287d2ac91c
thepowderguy <brandonli2010@gmail.com>
2014-06-06 21:01:05 -0400

    Update ZweiAndPowdersVotePlugin.java

--- src/main/java/io/github/thepowderguy/ZweiAndPowdersVotePlugin/ZweiAndPowdersVotePlugin.java
+++ src/main/java/io/github/thepowderguy/ZweiAndPowdersVotePlugin/ZweiAndPowdersVotePlugin.java
@@ -3,5 +3,5 @@ package io.github.thepowderguy.ZweiAndPowdersVotePlugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ZweiAndPowdersVotePlugin extends JavaPlugin {
-
+  public boolean onCommand(CommandSender sender, Command cmd, String label, String[]args)
 }
