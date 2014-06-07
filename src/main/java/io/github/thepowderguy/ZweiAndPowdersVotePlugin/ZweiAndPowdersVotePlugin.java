c2206b99c1a2f7cdaad0d374d6a5fdc241ff9974
2insanepeople <zcm727@gmail.com>
2014-06-06 21:18:09 -0500

    Update ZweiAndPowdersVotePlugin.java

--- src/main/java/io/github/thepowderguy/ZweiAndPowdersVotePlugin/ZweiAndPowdersVotePlugin.java
+++ src/main/java/io/github/thepowderguy/ZweiAndPowdersVotePlugin/ZweiAndPowdersVotePlugin.java
@@ -4,20 +4,32 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ZweiAndPowdersVotePlugin extends JavaPlugin {
-  public boolean onCommand(CommandSender sender, Command cmd, String label, String[]args)
-  {
-	  if (cmd.getName().equalsIgnoreCase("createvote"))
-	  {
-		  String[] choices = args[1].split(",");
-		  String[] disallowed;
-		  if (args[2].equalsIgnoreCase("none"))
-			  disallowed = null;
-		  else
-			  disallowed = args[2].split(",");
-		  long endtime = (long) ((System.currentTimeMillis() / 1000L) + (Double.valueOf(args[0]) * 3600));
-		  //TODO
-	  }
-	  return false;
-  }
+public final class ZweiAndPowdersVotePlugin extends JavaPlugin {
+	@Override
+	public void onEnable()
+	{
+		// We might need something here later
+	}
+	@Override
+	public void onDisable()
+	{
+		// We might need something here later
+	}
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{
+		if(cmd.getName().equalsIgnoreCase("vote"))
+		{
+			if(args.length == 0)
+			{
+				// Send line showing question based on current question in config
+				// PS add a way to store current question in config
+				// send player list of choices based off of a list of values in votes.n.choices
+				// only do the above if player is allowed in the vote. If player is disallowed, then
+				// tell them the question followed by "You cannot vote in this poll"
+				// after all this, return true.
+			}
+		}
+		return false;
+	}
 }
