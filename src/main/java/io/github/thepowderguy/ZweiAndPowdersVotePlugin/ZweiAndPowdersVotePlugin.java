ae65198abd828ec05b8a98573c272dec4b615967
thepowderguy <brandonli2010@gmail.com>
2014-06-07 08:40:26 -0400

    Update ZweiAndPowdersVotePlugin.java

--- src/main/java/io/github/thepowderguy/ZweiAndPowdersVotePlugin/ZweiAndPowdersVotePlugin.java
+++ src/main/java/io/github/thepowderguy/ZweiAndPowdersVotePlugin/ZweiAndPowdersVotePlugin.java
@@ -1,5 +1,8 @@
 package io.github.thepowderguy.ZweiAndPowdersVotePlugin;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -20,14 +23,44 @@ public final class ZweiAndPowdersVotePlugin extends JavaPlugin {
 	{
 		if(cmd.getName().equalsIgnoreCase("createvote"))
 		{
+			if(args.length < 4)
+			{
+				//sender sendmessage commandsyntax
+				return true;
+			}
+ 			long endtime;
+ 			try {
+ 				endtime = (long) ((System.currentTimeMillis() / 1000L) + (Double.valueOf(args[0]) * 3600));
+ 			} catch (NumberFormatException e) {
+ 				sender.sendMessage("Invalid numbutt");
+ 				return true;
+ 			}
 			String[] choices = args[1].split(",");
- +			String[] disallowed;
- +			if (args[2].equalsIgnoreCase("none"))
- +				disallowed = null;
- +			else
- +				disallowed = args[2].split(",");
- +			long endtime = (long) ((System.currentTimeMillis() / 1000L) + (Double.valueOf(args[0]) * 3600));
- +		  //TODO
+ 			List<String> disallowed;
+ 			if (args[2].equalsIgnoreCase("none"))
+ 				disallowed = null;
+ 			else
+ 				disallowed = Arrays.asList(args[2].split(","));
+ 			StringBuilder tempQuestion = new StringBuilder();
+ 			String question;
+ 			for (int i = 3; i < args.length; i++)
+ 			{
+ 				tempQuestion.append(args[i]);
+ 				tempQuestion.append(' ');
+ 			}
+ 			question = tempQuestion.substring(0, tempQuestion.length() - 2);
+ 			
+ 			
+ 			
+ 			String current = String.valueOf(this.getConfig().getInt("current-id") + 1);
+ 			this.getConfig().set("votes." + current + ".question", question);
+ 			this.getConfig().set("votes." + current + ".time-end", endtime);
+ 			this.getConfig().set("votes." + current + ".disallowed", disallowed);
+ 			for (String i : choices)
+ 			{
+ 				this.getConfig().createSection("votes." + current + ".choices." + i);
+ 			}
+ 			this.getConfig().set("current-id", this.getConfig().getInt("current-id") + 1);
 		}
 		if(cmd.getName().equalsIgnoreCase("vote"))
 		{
