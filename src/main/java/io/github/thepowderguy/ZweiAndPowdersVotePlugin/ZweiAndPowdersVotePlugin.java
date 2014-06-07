faf6b2cea8bb8ab372b04f63d1135441f7540dd4
thepowderguy <brandonli2010@gmail.com>
2014-06-06 21:09:37 -0400

    Update ZweiAndPowdersVotePlugin.java

--- src/main/java/io/github/thepowderguy/ZweiAndPowdersVotePlugin/ZweiAndPowdersVotePlugin.java
+++ src/main/java/io/github/thepowderguy/ZweiAndPowdersVotePlugin/ZweiAndPowdersVotePlugin.java
@@ -1,7 +1,16 @@
 package io.github.thepowderguy.ZweiAndPowdersVotePlugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ZweiAndPowdersVotePlugin extends JavaPlugin {
   public boolean onCommand(CommandSender sender, Command cmd, String label, String[]args)
+  {
+	  if (cmd.getName().equalsIgnoreCase("createvote"))
+	  {
+		  
+	  }
+	  return false;
+  }
 }
